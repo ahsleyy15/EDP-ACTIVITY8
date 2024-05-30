c086ffff73dd4ffd372c43b29567d5fb2f124769
Trisha Belen <trishaannesumanting.belen@bicol-u.edu.ph>
2024-05-30 20:33:00 +0800

    Add project files.

---
+++ EDP8/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using System.Windows.Forms;
+
+namespace EDP8
+{
+    public partial class Form1 : Form
+    {
+        private static readonly HttpClient client = new HttpClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtOutput.Clear();
+                HttpResponseMessage response = await client.GetAsync("http://localhost/phpapi/api.php");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                txtOutput.Text = responseBody;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private async void btnPost_Click(object sender, EventArgs e)
+        {
+            var userData = new { LastName = txtUsername.Text, FirstName = txtUsername.Text, password = txtPassword.Text, email = txtEmail.Text };
+            string json = JsonConvert.SerializeObject(userData);
+            HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            try
+            {
+                HttpResponseMessage response = await client.PostAsync("http://localhost/phpapi/api.php", content);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                txtOutput.Text = responseBody;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
